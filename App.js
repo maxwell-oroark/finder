1f04f57729f6f16d98cd69dff104274da375dd0f
Maxwell ORoark <m.d.oroark@gmail.com>
2023-12-25 11:01:09 -0700

    places api

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View, FlatList, Dimensions } from "react-native";
 import Card from "./Card";
 
 export default function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const PLACES_URL = "https://places.googleapis.com/v1/places:searchNearby";
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        locationRestriction: {
+          circle: {
+            center: {
+              latitude: 37.7937,
+              longitude: -122.3965,
+            },
+            radius: 500.0,
+          },
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": process.env.EXPO_PUBLIC_PLACES_API_KEY,
+      },
+    };
+    fetch(PLACES_URL, options)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  console.log({ data });
+
   const [restuarants, setRestuarants] = useState([
     {
       name: "Guidos",
       photos: [
         { name: "item 1", color: "red" },
-        { name: "item 2", color: "white" },
+        { name: "item 2", color: "#FFEFD5" },
         { name: "item 3", color: "blue" },
       ],
     },
@@ -20,6 +48,14 @@ export default function App() {
         { name: "item 6", color: "gold" },
       ],
     },
+    {
+      name: "Barbaros",
+      photos: [
+        { name: "item 7", color: "silver" },
+        { name: "item 8", color: "plum" },
+        { name: "item 9", color: "orange" },
+      ],
+    },
   ]);
 
   return (
@@ -29,6 +65,12 @@ export default function App() {
         renderItem={({ item }) => {
           return (
             <FlatList
+              initialScrollIndex={1}
+              getItemLayout={(data, index) => ({
+                length: Dimensions.get("window").width,
+                offset: Dimensions.get("window").width * index,
+                index,
+              })}
               data={item.photos}
               horizontal
               renderItem={({ item }) => (
@@ -45,6 +87,12 @@ export default function App() {
             />
           );
         }}
+        initialScrollIndex={1}
+        getItemLayout={(data, index) => ({
+          length: Dimensions.get("window").height,
+          offset: Dimensions.get("window").height * index,
+          index,
+        })}
         keyExtractor={(item) => item.name}
         snapToAlignment="start"
         decelerationRate="fast"
